e07b2885e22d87f294d15a08e5ba72a8a9d12f49
kazusumi <nishimura.tamaka@gmail.com>
2017-12-30 20:03:33 +0900

    敵の複数生産するところまで

--- classtest/components/script.js
+++ classtest/components/script.js
@@ -1,13 +1,32 @@
 enchant();
 
+Enemy = Class.create(Sprite,{
+	initialize: function(x,y,p){	//位置と、パターンを渡せるように引数設定
+		Sprite.call(this, 32, 32);	//第一引数はコンテキスト…スコープ、以降は必要な引数
+		this.x = x;
+		this.y = y;
+		this.image = game.assets['img/chara1.png'];
+		stage.addChild(this);	//グループに追加。グループは複数の子ノードを持てるらしい。
+	},
+	onenterframe: function(){
+		this.x -= 1;
+		this.y += Math.sin( this.age*0.1);
+	}
+
+});
+
 window.onload = function(){
-	var game = new Game(500, 500);
+	game = new Game(500, 500);
 	game.preload('img/chara1.png', 'img/ball.png');
 	game.onload = function(){
-		var star = new Sprite(32, 32);
-		star.image = game.assets['img/chara1.png'];
-		star.frame = 5;
-		game.rootScene.addChild(star);
+		stage = new Group();	//新しいグループを作成。インスタンスたちの受け皿になる予定？
+		game.rootScene.addChild( stage );	//↑を、シーンに追加
+		
+		game.addEventListener( 'enterframe', function(){	//毎フレーム実行
+			if ( game.frame%10 == 0){
+				var enemy = new Enemy( 320, Math.random()*320);	//
+			}
+		});
 	};
 	game.start();
 };
